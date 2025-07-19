2f606cccaaa62361fc095f9bad113642f61357a6
Luchintha Anjana <luchinthaa@gmail.com>
2025-07-19 15:35:05 +0530

    Header Section Done (Need to add dynamic section)

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ export default function App() {
 
   return (
     <>
-      <h1>Hello There </h1>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the 
+          programming world safe from Assembly</p>
+        <section className="status-bar">
+          <h2>You Win!</h2>
+          <p>Well Done!</p>
+        </section>
+      </header>
     </>
   )
 }
