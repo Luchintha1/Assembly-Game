04a234b6be25c9ab5478dc2c931096d3f70c552f
Luchintha Anjana <luchinthaa@gmail.com>
2025-07-19 22:44:22 +0530

    Add a wrong guess count variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,63 @@
+import { languages } from "./languages.js";
+import React from "react"
+import clsx from "clsx";
+
 export default function App() {
+
+  // States
+  const [currentWord, setCurrentWord] = React.useState("react");
+  const [guessedLetter, setGuessedLetter] = React.useState([]);
+
+  // Static Values
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+  // Derived Values
+  const letterArray = currentWord.split("");
+  const alphabetArray = alphabet.split("");
+
+  const languageElements = languages.map((lan) => {
+    return(
+      <span 
+        className="chip-element"
+        key={lan.name}
+        style={{backgroundColor: lan.backgroundColor, color: lan.color}}>
+          {lan.name}
+      </span>);
+  })
+
+  const letterElements = letterArray.map((letter, index) => {
+    return(
+      <span
+        key={index}
+        className="letter">
+        {guessedLetter.includes(letter) ? letter.toUpperCase() : ""}
+      </span>
+    )
+  })
+
+  const alphabetElements = alphabetArray.map((letter, index) => {
+    return(
+      <button
+        key={index}
+        onClick={()=>letterClicked(letter)}
+        className={
+          clsx("alphabet-button",
+          guessedLetter.includes(letter) && (currentWord.includes(letter) ? 
+          "alphabet-button-correct" : 
+          "alphabet-button-wrong"
+        ))}
+        >
+          {letter.toUpperCase()}
+        </button>
+    )
+  })
+
+  function letterClicked(letter){
+    setGuessedLetter(prevState => 
+      prevState.includes(letter) ? 
+      [...prevState] : 
+      [...prevState, letter]);
+  }
 
   return (
     <>
@@ -8,9 +67,19 @@ export default function App() {
           programming world safe from Assembly</p>
         <section className="status-bar">
           <h2>You Win!</h2>
-          <p>Well Done!</p>
+          <p>Well Done! 🎉</p>
         </section>
       </header>
+      <section className="chip-section">
+        {languageElements}
+      </section>
+      <section className="letter-section">
+        {letterElements}
+      </section>
+      <section className="keyboard">
+        {alphabetElements}
+      </section>
+      <button className="new-game">New Game</button>
     </>
   )
 }
