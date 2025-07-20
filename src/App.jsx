c1d504a4c8983dc858e4a5aa2865e0d6501edd95
Luchintha Anjana <luchinthaa@gmail.com>
2025-07-20 16:28:12 +0530

    Add winning and losing structures

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ export default function App() {
   const alphabetArray = alphabet.split("");
   const wrongGuessCount = guessedLetter.filter(letter => !currentWord.includes(letter)).length;
 
-
   const languageElements = languages.map((lan, index) => {
     return(
       <span 
@@ -26,6 +25,11 @@ export default function App() {
           {lan.name}
       </span>);
   })
+
+  const isGameWon = currentWord.split("").every(letter => guessedLetter.includes(letter));
+  const isGameLost = (wrongGuessCount >= (languageElements.length - 1)) ? true : false;
+
+  const isGameOver = isGameWon || isGameLost;
 
   const letterElements = letterArray.map((letter, index) => {
     return(
@@ -61,15 +65,40 @@ export default function App() {
       [...prevState, letter]);
   }
 
+  function renderGameStates(){
+    if(!isGameOver){
+      return null;
+    }
+
+    if(isGameWon){
+      return(
+      <>
+        <h2>You Win!</h2>
+        <p>Well done! 🎉</p>
+      </>);
+    }
+    else{
+      return(
+      <>
+        <h2>Game Over!</h2>
+        <p>You lose! Better start learning Assembly 😭</p>
+      </>);
+    }
+  }
+
   return (
     <>
       <header>
         <h1>Assembly: Endgame</h1>
         <p>Guess the word in under 8 attempts to keep the 
           programming world safe from Assembly</p>
-        <section className="status-bar">
-          <h2>You Win!</h2>
-          <p>Well Done! 🎉</p>
+        <section className=
+        { 
+          isGameWon ? "status-bar status-bar-won" : isGameLost ? 
+          "status-bar status-bar-lost" : 
+          "status-bar"
+          }>
+          {renderGameStates()}
         </section>
       </header>
       <section className="chip-section">
@@ -81,7 +110,7 @@ export default function App() {
       <section className="keyboard">
         {alphabetElements}
       </section>
-      <button className="new-game">New Game</button>
+      {isGameOver &&<button className="new-game">New Game</button>}
     </>
   )
 }
